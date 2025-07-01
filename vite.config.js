0219df1c2be132562628d17587cdb7f66445a75e
Csaba Molnar <108236828+DocForLoop@users.noreply.github.com>
2025-07-01 10:19:45 +0200

    Add basic CSS styles

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import autoprefixer from 'autoprefixer';
 export default defineConfig({
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, 'src'), // Set src folder as an alias
       '@scss': path.resolve(__dirname, 'src/scss'), // Set scss folder as an alias
     },
   },
